2a1fdf0a6935c4bda4608adad52642dff212eaab
behzod_709 <behzodsharifjonov7@gmail.com>
2024-06-23 11:43:01 +0500

    start home page

---
+++ src/component/header/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav>
+
+            <img src="" alt="Logo.png" />
+
+            <ul>
+                <NavLink>Модели</NavLink>
+                <NavLink>Экосистема</NavLink>
+                <NavLink>Возможности</NavLink>
+                <NavLink>Шаги</NavLink>
+                <NavLink>
+                    <button>
+                        <select name="" id="">
+                            <option value="uz">UZ</option>
+                            <option value="ru">RU</option>
+                        </select>
+                    </button>
+                </NavLink>
+            </ul>
+
+            <button>Войти</button>
+
+        </nav>
+    )
+}
+
+export default Navbar
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Home.css'
+import Header from '../../component/header/Header'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <div className="home-container">
+                <Header />
+            </div>
+        </div>
+    )
 }
 
 export default Home
